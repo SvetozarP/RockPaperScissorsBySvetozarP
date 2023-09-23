0d18bcdb849b1838ea3de558986e17a687ebb2f7
SvetozarP <145108913+SvetozarP@users.noreply.github.com>
2023-09-23 19:27:29 +0100

    Added scoring system

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -5,6 +5,9 @@ rock = "Rock"
 paper = "Paper"
 scissors = "Scissors"
 
+win: int = 0
+draw: int = 0
+lose: int = 0
 
 player_move = input("Choose [r]ock, [p]aper or [s]cissors or [q]uit: ")
 
@@ -36,10 +39,19 @@ while player_move != "q":
 
     if (player_move == rock and computer_move == scissors) or (player_move == paper and computer_move == rock) or \
             (player_move == scissors and computer_move == paper):
-        print(Fore.LIGHTGREEN_EX + "You win!")
+        print(Fore.LIGHTGREEN_EX + "You win!" + Fore.RESET)
+        win += 1
+        print("Scores so far: " + Fore.LIGHTGREEN_EX + f"Win: {win} " + Fore.YELLOW + f"Draw: {draw} " + Fore.RED
+              + f"Lose {lose} " + Fore.RESET)
     elif player_move == computer_move:
-        print(Fore.YELLOW + "Draw!")
+        print(Fore.YELLOW + "Draw!" + Fore.RESET)
+        draw += 1
+        print("Scores so far: " + Fore.LIGHTGREEN_EX + f"Win: {win} " + Fore.YELLOW + f"Draw: {draw} " + Fore.RED
+              + f"Lose {lose} " + Fore.RESET)
     else:
-        print(Fore.RED + "You lose!")
+        print(Fore.RED + "You lose!" + Fore.RESET)
+        lose += 1
+        print("Scores so far: " + Fore.LIGHTGREEN_EX + f"Win: {win} " + Fore.YELLOW + f"Draw: {draw} " + Fore.RED
+              + f"Lose {lose} " + Fore.RESET)
 
     player_move = input(Fore.RESET + "Choose [r]ock, [p]aper or [s]cissors or [q]uit: ")
